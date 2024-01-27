5b7cd969790b705665d3d4af6bd18cae587abdb8
tsii06 <tsiorydrage@gmail.com>
2024-01-27 05:27:10 +0100

    Update LoginController.java

--- src/main/java/com/example/vaika/controller/LoginController.java
+++ src/main/java/com/example/vaika/controller/LoginController.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
