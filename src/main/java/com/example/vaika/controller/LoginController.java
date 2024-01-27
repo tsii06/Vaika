203717b57f0c5afb7f7fddf5d4de2bb84bb77664
tsii06 <tsiorydrage@gmail.com>
2024-01-27 05:22:11 +0100

    Update LoginController.java

--- src/main/java/com/example/vaika/controller/LoginController.java
+++ src/main/java/com/example/vaika/controller/LoginController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class LoginController {
 
     private final UtilisateurService utilisateurService;
